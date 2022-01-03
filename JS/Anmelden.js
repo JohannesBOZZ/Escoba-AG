8474223404ca707ffd601ea804f10f7944d27651
JohannesBOZZ <maxb12032005@gmail.com>
2022-01-03 18:45:37 +0100

    main

---
+++ JS/Anmelden.js
@@ -0,0 +1,12 @@
+const email = document.getElementById("email");
+console.log( email)
+const passwort = document.getElementById("passwort");
+console.log( passwort)
+let a = 1
+let b = 0
+
+if (email == 'Escoba AG' && (passwort == 'Ben ist sus')) {
+    console.log(sus)
+} else {
+    console.log(b)
+};
