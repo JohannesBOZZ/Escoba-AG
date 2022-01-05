b22f2c0852059995e88b3e95a400f19f4851cb6e
JohannesBOZZ <maxb12032005@gmail.com>
2022-01-05 23:25:51 +0100

    main

--- JS/Anmelden.js
+++ JS/Anmelden.js
@@ -1,12 +1,14 @@
-const email = document.getElementById("email");
-console.log( email)
-const passwort = document.getElementById("passwort");
-console.log( passwort)
+const email = document.getElementById('email')
+console.log(email)
+const passswort = document.getElementById('passswort')
+console.log(passswort)
+const form = document.getElementById('form')
 let a = 1
 let b = 0
 
+
 if (email == 'Escoba AG' && (passwort == 'Ben ist sus')) {
-    console.log(sus)
+    console.log(a)
 } else {
     console.log(b)
 };
